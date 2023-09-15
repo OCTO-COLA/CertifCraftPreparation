0c20394cf49285bdd8db4776ae0a16582ba283cf
corentin.conte <corentin.conte@octo.com>
2023-09-15 14:37:49 +0200

    (all-in) clone of https://github.com/sandromancuso/trip-service-kata/tree/master

---
+++ trip-service-kata-master/TypeScript/src/trip/TripService.ts
@@ -0,0 +1,29 @@
+import UserNotLoggedInException from "../exception/UserNotLoggedInException";
+import User from "../user/User";
+import UserSession from "../user/UserSession";
+import Trip from "./Trip";
+import TripDAO from "./TripDAO";
+
+export default class TripService {
+    public getTripsByUser(user: User, loggedUser: User): Trip[] {
+        let tripList: Trip[] = [];
+        let isFriend: boolean = false;
+
+        if (loggedUser != null) {
+            for (const friend of user.getFriends()) {
+                if (friend === loggedUser) {
+                    isFriend = true;
+                    break;
+                }
+            }
+
+            if (isFriend) {
+                tripList = TripDAO.findTripsByUser(user);
+            }
+
+            return tripList;
+        } else {
+            throw new UserNotLoggedInException();
+        }
+    }
+}
